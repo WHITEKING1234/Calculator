c4982e91aa5f4f62b9c02e5a858ad1d256ec7443
bombag456 <bombag456@gmail.com>
2023-04-23 13:32:36 +0600

    add

---
+++ Calculator/model/ Operations .swift
@@ -0,0 +1,15 @@
+//
+//   Operations .swift
+//  Calculator
+//
+//  Created by Mac on 23/4/23.
+//
+
+import Foundation
+enum Operations {
+    case noAction
+    case addition
+    case substraction
+    case division
+    case multiplication
+}
---
+++ Calculator/model/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Calculator
+//
+//  Created by Mac on 23/4/23.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let screenRect    = UIScreen.main.bounds
+    static let screenWidth   = screenRect.size.width
+    static let screenHeight  = screenRect.size.height
+}
